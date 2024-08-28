e59e55df7fb41b48fd90aad665cc37892584f74c
girrajsinghal <girrajsinghal12@gmail.com>
2024-08-28 11:32:06 +0530

    data change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     methods: "GET,POST,PUT,DELETE",
-    credentials: true, // Allow cookies and authentication headers
+    credentials: true, // Allow cookies and authentication
   })
 );
 
