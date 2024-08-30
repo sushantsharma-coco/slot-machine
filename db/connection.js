0439028bc140bbd08157905c86e9bdf0bead6e6e
sushantsharma-coco <cocosingh456@gmail.com>
2024-08-30 12:10:55 +0530

    msg

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017", {});
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
     if (!conn) {
       console.error("DB connection unsuccessful");
